468571d1df1f5abfc2f0db3945a19e2be6f70aea
nao-pon <hypweb@gmail.com>
2014-02-09 14:53:44 +0900

    support "Legacy.Event.RegistUser.SetValidators"

--- html/modules/captcha/preload/preload.class.php
+++ html/modules/captcha/preload/preload.class.php
@@ -6,6 +6,7 @@ class captcha_preload extends XCube_ActionFilter
 {
 	function postFilter() {
 		$this->mRoot->mDelegateManager->add('Legacy.Event.RegistUser.Validate', array($this, 'registUserValidate'));
+		$this->mRoot->mDelegateManager->add('Legacy.Event.RegistUser.SetValidators', array($this, 'registUserSetValidator'));
 	}
 	
 	function registUserValidate($actionForm) {
@@ -18,5 +19,18 @@ class captcha_preload extends XCube_ActionFilter
 				$actionForm->addErrorMessage(_CAPTCHA_ERROR);
 			}
         }
+	}
+	
+	function registUserSetValidator(&$validators) {
+		$handler =& xoops_gethandler('config');
+        $mConfigs = $handler->getConfigsByDirname('captcha');
+        if ($mConfigs['registUser']) {
+			include_once XOOPS_ROOT_PATH.'/modules/captcha/include/api.php';
+			$captcha_api = captcha_api::getInstance();
+			$validators[] = array(
+				'caption' => $captcha_api->make_caption(),
+				'element' => $captcha_api->make_img_input()
+			);
+		}
 	}
 }
