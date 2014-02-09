a863d3e4da4f8e3d824f9ffb7056c325ee5e0e19
nao-pon <hypweb@gmail.com>
2014-02-09 19:42:45 +0900

    source code format

--- html/modules/captcha/preload/preload.class.php
+++ html/modules/captcha/preload/preload.class.php
@@ -11,14 +11,14 @@ class captcha_preload extends XCube_ActionFilter
 	
 	function registUserValidate($actionForm) {
 		$handler =& xoops_gethandler('config');
-        $mConfigs = $handler->getConfigsByDirname('captcha');
-        if ($mConfigs['registUser']) {
+		$mConfigs = $handler->getConfigsByDirname('captcha');
+		if ($mConfigs['registUser']) {
 			include_once XOOPS_ROOT_PATH.'/modules/captcha/include/api.php';
 			$captcha_api = captcha_api::getInstance();
 			if ( !$captcha_api->validate_post() ) {
 				$actionForm->addErrorMessage(_CAPTCHA_ERROR);
 			}
-        }
+		}
 	}
 	
 	function registUserSetValidator(&$validators) {
